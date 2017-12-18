30b18911168e7c6462ab08f55034f72708afd79c
Ahmed Salah <asalah417@gmail.com>
2017-12-18 19:39:44 +0200

    .

--- Chess1/src/chess/King.java
+++ Chess1/src/chess/King.java
@@ -20,29 +20,11 @@ public class King implements piece {
 
     @Override
     public void generate() {
-         point a = new point(pos.x,pos.y-1);
-     
-     if(canMove(a)){
-         posmoves.add(a);
-     }
-     
-         point b = new point(pos.x,pos.y+1);
-     
-     if(canMove(b)){
-         posmoves.add(b);
-     }
-     
-        point c= new point(pos.x+1,pos.y);
-     
-     if(canMove(c)){
-         posmoves.add(c);
-     }
-     
-        point d= new point(pos.x-1,pos.y);
-     
-     if(canMove(d)){
-         posmoves.add(d);
-     }
+        int arr[]={0,1,-1};
+        for(int i=0;i<3;i++)
+            for(int j=0;j<3;j++){
+                if(canMove(new point(pos.x+i,pos.y+j))&&(i|j)!=0)posmoves.add(new point(pos.x+i,pos.y+j));
+            }
     }
     
 }
