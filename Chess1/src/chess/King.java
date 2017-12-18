d96f44001c080f96b7d1705181994d316adc9796
amraymankamal <amrayman1996@yahoo.com>
2017-12-18 17:22:09 +0000

    King

--- Chess1/src/chess/King.java
+++ Chess1/src/chess/King.java
@@ -5,10 +5,44 @@
  */
 package chess;
 
-/**
- *
- * @author Amr Ayman
- */
-public class King {
+
+public class King implements piece {
+
+    @Override
+    public void move(point newPosition) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean canMove(point position) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void generate() {
+         point a = new point(pos.x,pos.y-1);
+     
+     if(canMove(a)){
+         posmoves.add(a);
+     }
+     
+         point b = new point(pos.x,pos.y+1);
+     
+     if(canMove(b)){
+         posmoves.add(b);
+     }
+     
+        point c= new point(pos.x+1,pos.y);
+     
+     if(canMove(c)){
+         posmoves.add(c);
+     }
+     
+        point d= new point(pos.x-1,pos.y);
+     
+     if(canMove(d)){
+         posmoves.add(d);
+     }
+    }
     
 }
